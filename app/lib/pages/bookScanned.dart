542bd1ddc7af13a91c739402023bcc75b5e4715d
Rafael David <rafaelcapitaodavid@gmail.com>
2024-04-20 19:28:12 +0100

    .

--- app/lib/pages/bookScanned.dart
+++ app/lib/pages/bookScanned.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:sign_in_button/sign_in_button.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
 class BookScannedPage extends StatefulWidget {
-  final AuthClient client;
-  const BookScannedPage({super.key, required this.client});
+  const BookScannedPage({super.key});
 
   @override
   State<BookScannedPage> createState() => _BookScannedState();
